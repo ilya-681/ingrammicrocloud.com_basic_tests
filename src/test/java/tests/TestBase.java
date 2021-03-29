b24f575454901f8e6208e7a62ef315d572378004
ilya <ilya-681@ya.ru>
2021-03-30 00:23:13 +0300

    Added TestBase and AttachmentsHelper

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,44 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import config.WebConfig;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Configuration.browser;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
+
+public class TestBase {
+    @BeforeAll
+    static void setup() {
+        final WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());
+        Configuration.startMaximized = true;
+        browser = config.getWebDriverBrowser();
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+
+        if (System.getProperty("remote_driver") != null || System.getProperty("env") != null) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            capabilities.setVersion(config.getWebDriverBrowserVersion());
+            Configuration.browserCapabilities = capabilities;
+
+            Configuration.remote = config.getWebDriverURL();
+        }
+    }
+
+    @AfterEach
+    public void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        if (System.getProperty("video_storage") != null)
+            attachVideo();
+        closeWebDriver();
+    }
+}
